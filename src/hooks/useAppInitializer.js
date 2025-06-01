ff8604acba224ab59c2286090266498776b4168a
ayrtonviola <153876697+ayrtonviola@users.noreply.github.com>
2025-06-01 00:48:50 -0300

    Update useAppInitializer.js

--- src/hooks/useAppInitializer.js
+++ src/hooks/useAppInitializer.js
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import supabase from '../lib/supabaseClient';
+import { supabase } from '../lib/supabaseClient';
 
 const useAppInitializer = (toast, isLoading) => {
   useEffect(() => {
